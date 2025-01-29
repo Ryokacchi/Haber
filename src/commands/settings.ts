c2456e5247914c176217501c45fe995203d492ee
Roman <108236042+romanbaba@users.noreply.github.com>
2025-01-29 21:17:19 +0300

    ✅ Added settings.ts Command

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -1,9 +1,10 @@
-import { ApplicationIntegrationType, InteractionContextType, PermissionsBitField } from "discord.js";
+import type { InputJsonValue } from "@prisma/client/runtime/library";
+import { ApplicationIntegrationType, InteractionContextType, PermissionsBitField, type ButtonInteraction, type ChannelSelectMenuInteraction, type MentionableSelectMenuInteraction, type RoleSelectMenuInteraction, type StringSelectMenuInteraction, type UserSelectMenuInteraction } from "discord.js";
 import { Command } from "../classes/command.js";
 import { prisma } from "../functions/dbConnection.js";
 import type { ServiceData } from "../interfaces/prisma.types.js";
 import { getServicesByServer } from "../utils/rows.js";
-import { SetupView } from "../utils/views.js";
+import { SetupView, SuccessEmbed, withoutAuthor } from "../utils/views.js";
 
 export default new Command({
   data: (builder) =>
@@ -15,9 +16,45 @@ export default new Command({
       .setContexts(InteractionContextType.Guild),
   async execute(interaction) {
     const server = await prisma.servers.findUnique({ where: { server: interaction.guild!.id }, select: { services: true } });
+    let services = (server?.services ?? []) as unknown as ServiceData[];
 
-    const services = (server?.services ?? []) as unknown as ServiceData[];
+    const message = await interaction.editReply({ embeds: [SetupView(interaction)], components: [getServicesByServer({ services, disabled: services.length === 0 })] });
 
-    await interaction.editReply({ embeds: [SetupView(interaction)], components: [getServicesByServer({ services })] });
+    /**
+     * Filters interactions to ensure they match the original user's ID and message ID.
+     * 
+     * This function checks whether an interaction originates from the same user who initiated
+     * the command and is tied to the same message. It is typically used as a filter for collectors.
+     * 
+     * @param {StringSelectMenuInteraction | UserSelectMenuInteraction | RoleSelectMenuInteraction | MentionableSelectMenuInteraction | ChannelSelectMenuInteraction | ButtonInteraction} i 
+     *   The interaction to be checked. This can be one of several interaction types, such as:
+     *   - StringSelectMenuInteraction
+     *   - UserSelectMenuInteraction
+     *   - RoleSelectMenuInteraction
+     *   - MentionableSelectMenuInteraction
+     *   - ChannelSelectMenuInteraction
+     *   - ButtonInteraction
+     * 
+     * @returns {boolean} Returns `true` if the interaction is from the same user and matches the message ID, otherwise `false`.
+     */
+    const filter = (i: StringSelectMenuInteraction | UserSelectMenuInteraction | RoleSelectMenuInteraction | MentionableSelectMenuInteraction | ChannelSelectMenuInteraction | ButtonInteraction): boolean => i.user.id === interaction.user.id && message.id === i.message.id;
+    const collector = message.createMessageComponentCollector({ filter, time: 60 * 1000 });
+
+    collector.on("collect", (i) => {
+      void (async function() {
+        if (!i.isStringSelectMenu()) return;
+
+        const serviceId = i.values[0] as unknown as string;
+        services = services.filter((service) => service.categoryId !== serviceId);
+
+        console.log({ serviceId, services });
+        
+        await prisma.servers.update({
+          where: { server: interaction.guild!.id },
+          data: { services: [ ...services as unknown as  InputJsonValue[] ] }
+        });
+        await i.update({ embeds: [SetupView(interaction), withoutAuthor(SuccessEmbed(interaction).setDescription("Yapılan değişiklikler başarıyla kaydedildi ve veritabanına kaydedildi."))], components: [getServicesByServer({ services, disabled: services.length === 0 })] });
+      })();
+    });
   }
 });
